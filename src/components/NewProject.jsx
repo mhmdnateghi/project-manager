7f46bd095f9e06b8865db4979109c47b3fb74e7c
mhmdnateghi <mo2.nateghi@gmail.com>
2025-06-09 17:55:55 +0330

    add NewProject component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,28 @@
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>لغو</button>
+        </li>
+        <li>
+          <button>ثبت</button>
+        </li>
+      </menu>
+      <div>
+        <p>
+          <label>عنوان</label>
+          <input />
+        </p>
+        <p>
+          <label>توضیح</label>
+          <textarea />
+        </p>
+        <p>
+          <label>تاریخ تحویل</label>
+          <input />
+        </p>
+      </div>
+    </div>
+  );
+}
