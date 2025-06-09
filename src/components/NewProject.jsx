a1cca7da56f2b47e4609038be87e41aefe5208df
mhmdnateghi <mo2.nateghi@gmail.com>
2025-06-09 19:05:41 +0330

    improve Input and NewProject components layout and styling

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,15 @@ import Input from "./Input.jsx";
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-start gap-4 my-4">
         <li>
-          <button>لغو</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            ثبت
+          </button>
         </li>
         <li>
-          <button>ثبت</button>
+          <button className="text-stone-800 hover:text-stone-950">لغو</button>
         </li>
       </menu>
       <div>
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -2,10 +2,12 @@ export default function ProjectsSidebar() {
   return (
     <>
       <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-        <h2 className="mb-8 font-bold md:text-xl text-stone-200">پروژه ها</h2>
-        <div>
+        <h2 className="mb-8 font-bold md:text-xl text-stone-200 text-right">
+          پروژه ها
+        </h2>
+        <div className="flex justify-end">
           <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-            اضافه کردن
+            + اضافه کردن
           </button>
         </div>
       </aside>
