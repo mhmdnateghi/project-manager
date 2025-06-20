2870d0e4b24cc6f35936a3f11bb36edac49c3434
mhmdnateghi <mo2.nateghi@gmail.com>
2025-06-20 15:14:09 +0330

    improving input and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,36 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
+
   let content;
   if (projectState.selectedProjectID === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectState.selectedProjectID === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+        <ProjectsSidebar
+          onStartAddProject={handleStartAddProject}
+          projects={projectState.projects}
+        />
         {content}
       </main>
     </>
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,11 +1,31 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-start gap-4 my-4">
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             ثبت
           </button>
         </li>
@@ -14,9 +34,9 @@ export default function NewProject() {
         </li>
       </menu>
       <div>
-        <Input label="عنوان" />
-        <Input label="توضیح" textarea />
-        <Input label="تاریخ تحویل" />
+        <Input type="text" ref={title} label="عنوان" />
+        <Input ref={description} label="توضیح" textarea />
+        <Input type="date" ref={dueDate} label="تاریخ تحویل" />
       </div>
     </div>
   );
