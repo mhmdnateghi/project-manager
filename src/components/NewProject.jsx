168ed664da4ccd94b9e989e03cfc458f2fbb14bc
mhmdnateghi <mo2.nateghi@gmail.com>
2025-06-21 16:03:16 +0330

    add modal component and validation for NewProject form

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,27 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({ onAdd }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
+  const modal = useRef();
+
   function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -19,25 +31,32 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-start gap-4 my-4">
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            ثبت
-          </button>
-        </li>
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">لغو</button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="عنوان" />
-        <Input ref={description} label="توضیح" textarea />
-        <Input type="date" ref={dueDate} label="تاریخ تحویل" />
+    <>
+      <Modal ref={modal}>
+        <h1>Invalid Input</h1>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-start gap-4 my-4">
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              ثبت
+            </button>
+          </li>
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">لغو</button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="عنوان" />
+          <Input ref={description} label="توضیح" textarea />
+          <Input type="date" ref={dueDate} label="تاریخ تحویل" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
